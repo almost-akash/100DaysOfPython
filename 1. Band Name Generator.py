e2918506c8c7446eb840be44795354b629175bf1
Akash Sahu <asahu54007@gmail.com>
2024-06-12 18:24:01 +0530

    Day 3 - Treasure Island

--- 1. Band Name Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator")
-city = input("Which City did you grew up in?\n")
-pet = input("What was your Pet name?\n")
-print("Your Band Name could be " + city + " " + pet)
